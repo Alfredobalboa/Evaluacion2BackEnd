4999382473fcbcc5603f91fc061d66a47b44aaa3
Alfredo Balboa <alfredonachobalboa@gmail.com>
2023-10-23 16:18:46 -0300

    segunda app terminada faltan las vistas

--- evaluacion2/settings.py
+++ evaluacion2/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'primeraApp',
+    'segundaApp'
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_DIRS = [os.path.join(BASE_DIR, '/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- evaluacion2/urls.py
+++ evaluacion2/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from primeraApp.views import renderView
+from segundaApp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", renderView)
+    path("", renderView),
+    path("inicio", inicio)
+    
 ]
